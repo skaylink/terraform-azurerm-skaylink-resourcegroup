8f53e3d2bf39d0c0fb985c574cba16bbd11da66b
Mikael Lognseth <mlogn@icloud.com>
2022-05-25 15:50:59 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+# A Terraform module to create a subset of cloud components
+# Copyright (C) 2022 IQ3 CLOUD Skaylink GmbH
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# For questions and contributions please contact info@iq3cloud.com
+# https://github.com/iq3cloud-dev/terraform-azurerm-iq3-vnet
+
+resource "azurerm_resource_group" "resourcegroup" {
+  name     = var.name
+  location = var.location
+  tags = {
+    customTag1 = var.customTag1
+    customTag2 = var.customTag2
+    customTag3 = var.customTag3
+    customTag4 = var.customTag4
+    customTag5 = var.customTag5
+  }
+}
